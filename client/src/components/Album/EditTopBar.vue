14c78310d67a976a769a7ad29965395a1407b605
Seba2471 <siejak.sebastian@gmail.com>
2022-01-08 18:53:29 +0100

    Add edit album top bar

---
+++ client/src/components/Album/EditTopBar.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-row class="d-flex align-center justify-end">
+    <v-col offset-xl="7" class="d-flex align-center justify-end">
+      <v-col cols="4" md="3" xl="6">
+        <v-row>
+          <v-col class="customButton pa-3 d-flex align-center justify-center" @click="checkAllImages">
+            <v-icon> mdi-check-circle-outline </v-icon>
+            <span class="ml-3">Zaznacz wszystko</span>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="4" md="3" xl="6">
+        <v-row>
+          <v-col class="customButton pa-3 d-flex align-center justify-center" @click="uncheckAllImages">
+            <v-icon> mdi-checkbox-blank-circle-outline</v-icon>
+            <span class="ml-3">Odznacz wszystko</span>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="2" xl="3" v-if="!mobile && isSelected">
+        <v-row>
+          <v-col class="customButton pa-3 d-flex align-center justify-center" @click="deleteSelectedImages">
+            <v-icon> mdi-trash-can-outline </v-icon>
+            <span class="ml-3">Usuń</span>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="2" xl="3">
+        <v-row>
+          <v-col class="customButton pa-3 d-flex align-center justify-center" @click="disableEdit()">
+            <v-icon> mdi-arrow-u-left-top</v-icon>
+            <span class="ml-3">Wróć</span>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col sm="2" md="1" v-if="mobile && isSelected" class="d-flex align-center justify-center">
+        <v-menu left offset-y transition="scale-transition">
+          <template v-slot:activator="{ on, attrs }"><v-icon v-bind="attrs" v-on="on">mdi-dots-vertical</v-icon> </template>
+          <v-list>
+            <v-list-item>
+              <v-col class="customButton pa-3 d-flex align-center justify-center" @click="deleteSelectedImages">
+                <v-icon> mdi-trash-can-outline </v-icon>
+                <span class="ml-3">Usuń</span>
+              </v-col>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-col>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapMutations, mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'ImgTopBar',
+  props: ['images'],
+  data() {
+    return {
+      isSelected: false,
+      mobile: false,
+    };
+  },
+
+  created() {
+    this.fetchAlbums();
+    this.setSelected([]);
+  },
+  watch: {
+    selected: function (val) {
+      if (val.length == 0) {
+        this.isSelected = false;
+      } else {
+        this.isSelected = true;
+      }
+    },
+    windowWidth: async function () {
+      this.mobile = await this.isMobile(1200);
+    },
+  },
+  computed: {
+    ...mapGetters({
+      windowWidth: 'getWidth',
+      selected: 'getSelected',
+      albums: 'getAlbums',
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      setSelected: 'setSelected',
+    }),
+    ...mapActions({
+      deleteImage: 'deleteImage',
+      fetchAlbums: 'fetchAlbums',
+      isMobile: 'isMobile',
+    }),
+    disableEdit() {
+      this.$emit('disableEdit', false);
+    },
+    checkAllImages() {
+      const result = this.images.map((a) => a._id);
+      this.setSelected(result);
+    },
+    uncheckAllImages() {
+      this.setSelected([]);
+    },
+    deleteSelectedImages() {
+      this.$confirm('Czy na pewno chcesz usunąć?').then(() => {
+        this.deleteImage();
+      });
+    },
+  },
+};
+</script>
+<style>
+.sortButton {
+  margin: 0 0;
+  padding: 0 0;
+}
+</style>
--- client/src/views/Album.vue
+++ client/src/views/Album.vue
@@ -1,29 +1,36 @@
 <template>
   <div>
-    <v-row class="d-flex align-center justify-end mr-10 mt-2">
-      <v-col class="ml-5">
-        <h1>{{ album.name }}</h1>
-      </v-col>
-      <v-col cols="1" class="customButton d-flex align-center justify-center">
+    <v-row class="d-flex align-center justify-start mt-2">
+      <v-col v-if="!isEdit" @click="edit" cols="1" class="customButton d-flex align-center justify-center">
         <v-icon> mdi-sort</v-icon>
         <span class="ml-3">Edytuj </span>
       </v-col>
+      <v-col cols="12" xl="10" offset-xl="1">
+        <EditTopBar v-if="isEdit" :images="this.album.images" @disableEdit="disableEdit" />
+      </v-col>
     </v-row>
-    <ImgGrid :images="this.album.images" imgHeight="250px" />
+    <v-row>
+      <v-col cols="12" class="d-flex align-center justify-center">
+        <h1>{{ album.name }}</h1>
+      </v-col>
+    </v-row>
+    <ImgGrid class="d-flex align-center justify-start" :images="this.album.images" imgHeight="250px" />
   </div>
 </template>
 
 <script>
+import EditTopBar from '../components/Album/EditTopBar.vue';
 import ImgGrid from '../components/Images/ImgGrid.vue';
 import { mapActions, mapGetters, mapMutations } from 'vuex';
 export default {
-  components: { ImgGrid },
+  components: { ImgGrid, EditTopBar },
   name: 'Album',
   props: ['id'],
   data() {
     return {
       album: {},
       selectMode: false,
+      isEdit: false,
     };
   },
   created() {
@@ -55,6 +62,12 @@ export default {
     ...mapActions({
       fetchAlbums: 'fetchAlbums',
     }),
+    disableEdit(val) {
+      this.isEdit = val;
+    },
+    edit() {
+      this.isEdit = true;
+    },
   },
 };
 </script>
